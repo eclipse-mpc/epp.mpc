b5af74ef9c0a7f69246306598528b4400c9d9722
David Green <dgreen99@gmail.com>
2010-03-13 12:30:22 -0800

    support multiple provisioning operations

---
+++ org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/wizards/Operation.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2010 The Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     The Eclipse Foundation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.epp.internal.mpc.ui.wizards;
+
+/**
+ * Represents kinds of provisioning operations supported by the wizard
+ * 
+ * @author David Green
+ */
+public enum Operation {
+	INSTALL, UNINSTALL, CHECK_FOR_UPDATES, NONE
+}
