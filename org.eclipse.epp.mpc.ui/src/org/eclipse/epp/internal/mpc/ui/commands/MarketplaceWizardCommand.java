4b1af25c39fe9b3667a46f21b10e4398664c6635
Carsten Reckord <reckord@yatta.de>
2017-03-16 18:16:10 +0100

    493913: Add drag&drop support for favorites list URL
    Command to open Favorites import directly for a given URL
    
    Bug: 493913
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=493913

--- org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/commands/MarketplaceWizardCommand.java
+++ org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/commands/MarketplaceWizardCommand.java
@@ -13,7 +13,6 @@
  *******************************************************************************/
 package org.eclipse.epp.internal.mpc.ui.commands;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,22 +20,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.epp.internal.mpc.core.MarketplaceClientCore;
-import org.eclipse.epp.internal.mpc.ui.CatalogRegistry;
-import org.eclipse.epp.internal.mpc.ui.MarketplaceClientUi;
-import org.eclipse.epp.internal.mpc.ui.MarketplaceClientUiPlugin;
 import org.eclipse.epp.internal.mpc.ui.catalog.MarketplaceCatalog;
-import org.eclipse.epp.internal.mpc.ui.catalog.ResourceProvider;
 import org.eclipse.epp.internal.mpc.ui.wizards.AbstractTagFilter;
 import org.eclipse.epp.internal.mpc.ui.wizards.ComboTagFilter;
 import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceCatalogConfiguration;
@@ -44,35 +30,22 @@ import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceFilter;
 import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceWizard;
 import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceWizard.WizardState;
 import org.eclipse.epp.internal.mpc.ui.wizards.MarketplaceWizardDialog;
-import org.eclipse.epp.mpc.core.model.ICatalog;
 import org.eclipse.epp.mpc.core.model.ICategory;
 import org.eclipse.epp.mpc.core.model.IMarket;
-import org.eclipse.epp.mpc.core.service.ICatalogService;
-import org.eclipse.epp.mpc.core.service.ServiceHelper;
-import org.eclipse.epp.mpc.ui.CatalogDescriptor;
 import org.eclipse.epp.mpc.ui.IMarketplaceClientConfiguration;
 import org.eclipse.epp.mpc.ui.Operation;
-import org.eclipse.equinox.internal.p2.discovery.DiscoveryCore;
 import org.eclipse.equinox.internal.p2.discovery.model.Tag;
 import org.eclipse.equinox.internal.p2.ui.discovery.util.WorkbenchUtil;
 import org.eclipse.equinox.internal.p2.ui.discovery.wizards.CatalogFilter;
-import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.equinox.internal.p2.ui.discovery.wizards.DiscoveryWizard;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.statushandlers.StatusManager;
 
 /**
  * @author David Green
  * @author Carsten Reckord
  */
-public class MarketplaceWizardCommand extends AbstractHandler implements IHandler {
-
-	private List<CatalogDescriptor> catalogDescriptors;
-
-	private CatalogDescriptor selectedCatalogDescriptor;
+public class MarketplaceWizardCommand extends AbstractMarketplaceWizardCommand implements IHandler {
 
 	private String wizardState;
 
@@ -80,65 +53,9 @@ public class MarketplaceWizardCommand extends AbstractHandler implements IHandle
 
 	private WizardState wizardDialogState;
 
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final MarketplaceCatalog catalog = new MarketplaceCatalog();
-
-		catalog.setEnvironment(DiscoveryCore.createEnvironment());
-		catalog.setVerifyUpdateSiteAvailability(false);
-
-		MarketplaceCatalogConfiguration configuration = new MarketplaceCatalogConfiguration();
-		configuration.setVerifyUpdateSiteAvailability(false);
-
-		if (catalogDescriptors == null || catalogDescriptors.isEmpty()) {
-			final IStatus remoteCatalogStatus = installRemoteCatalogs();
-			configuration.getCatalogDescriptors().addAll(CatalogRegistry.getInstance().getCatalogDescriptors());
-			if (configuration.getCatalogDescriptors().isEmpty()) {
-				// doesn't make much sense to continue without catalogs.
-				// nothing will work and no way to recover later
-				IStatus cause;
-				if (!remoteCatalogStatus.isOK()) {
-					cause = remoteCatalogStatus;
-				} else {
-					cause = new Status(IStatus.ERROR, MarketplaceClientUi.BUNDLE_ID,
-							Messages.MarketplaceWizardCommand_noRemoteCatalogs);
-				}
-				IStatus exitStatus = new Status(IStatus.ERROR, MarketplaceClientUi.BUNDLE_ID, cause.getCode(),
-						Messages.MarketplaceWizardCommand_cannotOpenMarketplace, new CoreException(cause));
-				try {
-					MarketplaceClientUi.handle(exitStatus,
-							StatusManager.SHOW | StatusManager.BLOCK
-							| (exitStatus.getSeverity() == IStatus.CANCEL ? 0 : StatusManager.LOG));
-				} catch (Exception ex) {
-					// HOTFIX for bug 477269 - Display might get disposed during call to handle due to workspace shutdown or similar.
-					// In that case, just log...
-					MarketplaceClientUi.getLog().log(exitStatus);
-				}
-				return null;
-			} else if (!remoteCatalogStatus.isOK()) {
-				MarketplaceClientUi.handle(remoteCatalogStatus, StatusManager.LOG);
-			}
-		} else {
-			configuration.getCatalogDescriptors().addAll(catalogDescriptors);
-		}
-		if (selectedCatalogDescriptor != null) {
-			if (selectedCatalogDescriptor.getLabel().equals("org.eclipse.epp.mpc.descriptorHint")) { //$NON-NLS-1$
-				CatalogDescriptor resolvedDescriptor = CatalogRegistry.getInstance().findCatalogDescriptor(
-						selectedCatalogDescriptor.getUrl().toExternalForm());
-				if (resolvedDescriptor == null) {
-					IStatus status = new Status(IStatus.ERROR, MarketplaceClientUi.BUNDLE_ID,
-							Messages.MarketplaceWizardCommand_CouldNotFindMarketplaceForSolution, new ExecutionException(
-									selectedCatalogDescriptor.getUrl().toExternalForm()));
-					MarketplaceClientUi.handle(status,
-							StatusManager.SHOW | StatusManager.BLOCK | StatusManager.LOG);
-					return null;
-				} else {
-					configuration.setCatalogDescriptor(resolvedDescriptor);
-				}
-			} else {
-				configuration.setCatalogDescriptor(selectedCatalogDescriptor);
-			}
-		}
-
+	@Override
+	protected MarketplaceCatalogConfiguration createConfiguration(final MarketplaceCatalog catalog) {
+		MarketplaceCatalogConfiguration configuration = super.createConfiguration(catalog);
 		configuration.getFilters().clear();
 
 		final ComboTagFilter marketFilter = new ComboTagFilter() {
@@ -190,15 +107,21 @@ public class MarketplaceWizardCommand extends AbstractHandler implements IHandle
 		for (CatalogFilter filter : configuration.getFilters()) {
 			((MarketplaceFilter) filter).setCatalog(catalog);
 		}
+		return configuration;
+	}
 
+	@Override
+	protected MarketplaceWizardDialog createWizardDialog(DiscoveryWizard wizard) {
+		return new MarketplaceWizardDialog(WorkbenchUtil.getShell(), (MarketplaceWizard) wizard);
+	}
+
+	@Override
+	protected MarketplaceWizard createWizard(final MarketplaceCatalog catalog,
+			MarketplaceCatalogConfiguration configuration) {
 		MarketplaceWizard wizard = new MarketplaceWizard(catalog, configuration);
 		wizard.setInitialState(wizardDialogState);
 		wizard.setWindowTitle(Messages.MarketplaceWizardCommand_eclipseMarketplace);
-
-		WizardDialog dialog = new MarketplaceWizardDialog(WorkbenchUtil.getShell(), wizard);
-		dialog.open();
-
-		return null;
+		return wizard;
 	}
 
 	private void updateCategoryChoices(final ComboTagFilter marketCategoryTagFilter, final ComboTagFilter marketFilter) {
@@ -231,14 +154,6 @@ public class MarketplaceWizardCommand extends AbstractHandler implements IHandle
 		marketCategoryTagFilter.setChoices(choices);
 	}
 
-	public void setCatalogDescriptors(List<CatalogDescriptor> catalogDescriptors) {
-		this.catalogDescriptors = catalogDescriptors;
-	}
-
-	public void setSelectedCatalogDescriptor(CatalogDescriptor selectedCatalogDescriptor) {
-		this.selectedCatalogDescriptor = selectedCatalogDescriptor;
-	}
-
 	public void setWizardState(String wizardState) {
 		this.wizardState = wizardState;
 	}
@@ -266,82 +181,4 @@ public class MarketplaceWizardCommand extends AbstractHandler implements IHandle
 		setSelectedCatalogDescriptor(configuration.getCatalogDescriptor());
 	}
 
-	public IStatus installRemoteCatalogs() {
-		try {
-			final AtomicReference<List<? extends ICatalog>> result = new AtomicReference<List<? extends ICatalog>>();
-
-			PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
-				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-					try {
-						ICatalogService catalogService = ServiceHelper.getMarketplaceServiceLocator()
-								.getCatalogService();
-						final List<? extends ICatalog> catalogs = catalogService.listCatalogs(monitor);
-						result.set(catalogs);
-					} catch (CoreException e) {
-						if (e.getStatus().getSeverity() == IStatus.CANCEL) {
-							throw new InterruptedException();
-						}
-						throw new InvocationTargetException(e);
-					}
-				}
-			});
-
-			List<? extends ICatalog> catalogs = result.get();
-			for (ICatalog catalog : catalogs) {
-				ResourceProvider resourceProvider = MarketplaceClientUiPlugin.getInstance().getResourceProvider();
-				String catalogName = catalog.getName();
-				String requestSource = NLS.bind(Messages.MarketplaceWizardCommand_requestCatalog, catalogName,
-						catalog.getId());
-				String catalogImageUrl = catalog.getImageUrl();
-				if (catalogImageUrl != null) {
-					try {
-						resourceProvider.retrieveResource(requestSource, catalogImageUrl);
-					} catch (Exception e) {
-						MarketplaceClientUi.log(IStatus.WARNING,
-								Messages.MarketplaceWizardCommand_FailedRetrievingCatalogImage, catalogName,
-								catalogImageUrl, e);
-					}
-				}
-				if (catalog.getBranding() != null && catalog.getBranding().getWizardIcon() != null) {
-					String wizardIconUrl = catalog.getBranding().getWizardIcon();
-					try {
-						resourceProvider.retrieveResource(requestSource, wizardIconUrl);
-					} catch (Exception e) {
-						MarketplaceClientUi.log(IStatus.WARNING,
-								Messages.MarketplaceWizardCommand_FailedRetrievingCatalogWizardIcon, catalogName,
-								wizardIconUrl, e);
-					}
-				}
-				CatalogDescriptor descriptor = new CatalogDescriptor(catalog);
-				registerOrOverrideCatalog(descriptor);
-			}
-		} catch (InterruptedException ie) {
-			if (ie.getMessage() == null || "".equals(ie.getMessage())) {
-				InterruptedException ie1 = new InterruptedException("Operation cancelled");
-				ie1.setStackTrace(ie.getStackTrace());
-				if (ie.getCause() != null) {
-					ie1.initCause(ie.getCause());
-				}
-				ie = ie1;
-			}
-			IStatus errorStatus = MarketplaceClientCore.computeStatus(ie,
-					Messages.MarketplaceWizardCommand_CannotInstallRemoteLocations);
-			return new Status(IStatus.CANCEL, MarketplaceClientCore.BUNDLE_ID, errorStatus.getMessage(), ie);
-		} catch (Exception e) {
-			IStatus status = MarketplaceClientCore.computeStatus(e, Messages.MarketplaceWizardCommand_CannotInstallRemoteLocations);
-			return status;
-		}
-		return Status.OK_STATUS;
-	}
-
-	private void registerOrOverrideCatalog(CatalogDescriptor descriptor) {
-		CatalogRegistry catalogRegistry = CatalogRegistry.getInstance();
-		List<CatalogDescriptor> descriptors = catalogRegistry.getCatalogDescriptors();
-		for (CatalogDescriptor catalogDescriptor : descriptors) {
-			if (catalogDescriptor.getUrl().toExternalForm().equals(descriptor.getUrl().toExternalForm())) {
-				catalogRegistry.unregister(catalogDescriptor);
-			}
-		}
-		catalogRegistry.register(descriptor);
-	}
 }
