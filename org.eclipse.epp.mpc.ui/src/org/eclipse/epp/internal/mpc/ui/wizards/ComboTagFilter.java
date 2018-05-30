a24275da502a943c4d7e1a8f59f1bd600d7e3fc7
Carsten Reckord <reckord@yatta.de>
2018-05-30 21:20:31 +0200

    535037: [Gtk3] Categories combo layout broken
    Workaround: Use CCombo on Linux/GTK
    
    Bug: 535037
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=535037

--- org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/wizards/ComboTagFilter.java
+++ org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/wizards/ComboTagFilter.java
@@ -15,12 +15,15 @@ package org.eclipse.epp.internal.mpc.ui.wizards;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.internal.p2.discovery.model.Tag;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 /**
  * A tag filter that presents choices as a combo box. This filter can accomodate changes to the {@link #getChoices()
@@ -30,7 +33,7 @@ import org.eclipse.swt.widgets.Composite;
  */
 public class ComboTagFilter extends AbstractTagFilter {
 
-	private Combo combo;
+	private ComboWrapper combo;
 
 	private String noSelectionLabel;
 
@@ -41,7 +44,8 @@ public class ComboTagFilter extends AbstractTagFilter {
 		if (getChoices() == null) {
 			throw new IllegalStateException();
 		}
-		combo = new Combo(parent, SWT.READ_ONLY | SWT.DROP_DOWN);
+		combo = useCCombo() ? new CComboComboWrapper(parent, SWT.READ_ONLY | SWT.DROP_DOWN)
+				: new ComboComboWrapper(parent, SWT.READ_ONLY | SWT.DROP_DOWN);
 		combo.setData(this);
 		listener = new SelectionListener() {
 			@Override
@@ -62,6 +66,16 @@ public class ComboTagFilter extends AbstractTagFilter {
 		};
 		combo.addSelectionListener(listener);
 		rebuildChoicesUi();
+	}
+
+	/**
+	 * Workaround for MPC bug 535037 / SWT bug 517590: Plain Combo has wrong width in GTK3 if one of its entries is
+	 * wider than the combo itself. We could just replace the Combo with a CCombo globally, but until we can also get
+	 * more control about the rendering of the search text field, this looks strange on the other platforms. So we just
+	 * exchange it on Linux/GTK3 for now.
+	 */
+	private static boolean useCCombo() {
+		return Platform.WS_GTK.equals(Platform.getWS());
 	}
 
 	protected void rebuildChoicesUi() {
@@ -111,5 +125,135 @@ public class ComboTagFilter extends AbstractTagFilter {
 		}
 		combo.select(index + 1);//offset+1 for "All Markets" entry
 		super.updateUi();
+	}
+
+	private static abstract class ComboWrapper {
+		private final Control combo;
+
+		public ComboWrapper(Composite parent, int style) {
+			combo = createCombo(parent, style);
+		}
+
+		abstract Control createCombo(Composite parent, int style);
+
+		public Control getCombo() {
+			return combo;
+		}
+
+		public void setData(Object data) {
+			combo.setData(data);
+		}
+
+		public boolean isDisposed() {
+			return combo.isDisposed();
+		}
+
+		public abstract void select(int i);
+
+		public abstract void add(String element);
+
+		public abstract void removeAll();
+
+		public abstract void removeSelectionListener(SelectionListener listener);
+
+		public abstract void addSelectionListener(SelectionListener listener);
+
+		public abstract int getSelectionIndex();
+
+	}
+
+	private static class ComboComboWrapper extends ComboWrapper {
+
+		public ComboComboWrapper(Composite parent, int style) {
+			super(parent, style);
+		}
+
+		@Override
+		Control createCombo(Composite parent, int style) {
+			return new Combo(parent, style);
+		}
+
+		@Override
+		public Combo getCombo() {
+			return (Combo) super.getCombo();
+		}
+
+		@Override
+		public void select(int i) {
+			getCombo().select(i);
+		}
+
+		@Override
+		public void add(String element) {
+			getCombo().add(element);
+		}
+
+		@Override
+		public void removeAll() {
+			getCombo().removeAll();
+		}
+
+		@Override
+		public void removeSelectionListener(SelectionListener listener) {
+			getCombo().removeSelectionListener(listener);
+		}
+
+		@Override
+		public void addSelectionListener(SelectionListener listener) {
+			getCombo().addSelectionListener(listener);
+		}
+
+		@Override
+		public int getSelectionIndex() {
+			return getCombo().getSelectionIndex();
+		}
+	}
+
+	private static class CComboComboWrapper extends ComboWrapper {
+
+		public CComboComboWrapper(Composite parent, int style) {
+			super(parent, style);
+		}
+
+		@Override
+		Control createCombo(Composite parent, int style) {
+			return new CCombo(parent, style);
+		}
+
+		@Override
+		public CCombo getCombo() {
+			return (CCombo) super.getCombo();
+		}
+
+		@Override
+		public void select(int i) {
+			getCombo().select(i);
+		}
+
+		@Override
+		public void add(String element) {
+			getCombo().add(element);
+		}
+
+		@Override
+		public void removeAll() {
+			getCombo().removeAll();
+		}
+
+		@Override
+		public void removeSelectionListener(SelectionListener listener) {
+			getCombo().removeSelectionListener(listener);
+		}
+
+		@Override
+		public void addSelectionListener(SelectionListener listener) {
+			getCombo().addSelectionListener(listener);
+		}
+
+		@Override
+		public int getSelectionIndex() {
+			return getCombo().getSelectionIndex();
+		}
+
 	}
 }
