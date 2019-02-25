8f7c22775418929b51f9fa4feacd712bf6c355e4
Leif Geiger <geiger@yatta.de>
2019-02-25 20:00:17 +0100

    511447: IllegalArgumentException below NewsViewer.createNoBrowserPart
    Added additional check since widget might
    be disposed after createBrowser call.
    
    Change-Id: Ie7b9beaa021e591f092e8ad6e813baaf198c2e1b
    Signed-off-by: Leif Geiger <geiger@yatta.de>

--- org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/wizards/NewsViewer.java
+++ org.eclipse.epp.mpc.ui/src/org/eclipse/epp/internal/mpc/ui/wizards/NewsViewer.java
@@ -66,7 +66,7 @@ public class NewsViewer {
 
 	public Control createControl(Composite parent) {
 		control = createBrowser(parent);
-		if (control == null) {
+		if (control == null && !parent.isDisposed()) {
 			control = createNoBrowserPart(parent);
 		}
 		return control;
